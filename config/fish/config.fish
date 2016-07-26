2c8a5a7e32ee12ecaef75dee1cab0e32ad33dda0
Darcy Brown <darcy@darcybrown.com>
2016-07-26 15:18:35 -0500

    top sorts by cpu

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -60,6 +60,7 @@ alias sshfp1n='ssh frontpoint@tm26-s00059  -p 7000'
 alias sshfp2n='ssh frontpoint@tm26-s00060  -p 7001'
 alias sshfpdb1n='ssh frontpoint@tm26-s00062  -p 7003'
 alias sshfpsn='ssh frontpoint@tm26-s00058  -p 7000'
+alias top='top -o cpu'
 
 ulimit -n 1024
 eval (direnv hook fish)
