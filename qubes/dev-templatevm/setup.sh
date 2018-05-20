fb9cb46d88f2696b6b38187a9aff6e312633518a
Darcy Brown <darcy@darcybrown.com>
2018-05-20 12:53:28 -0500

    updated template script

--- qubes/dev-templatevm/setup.sh
+++ qubes/dev-templatevm/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+sudo yum groupinstall -y 'Development Tools'
+sudo dnf update -y
 sudo dnf config-manager --add-repo http://download.opensuse.org/repositories/shells:fish:release:2/Fedora_25/shells:fish:release:2.repo
 #sudo dnf -y copr enable dperson/neovim
 sudo dnf install -y curl wget git irb python-setuptools ruby zsh tmux htop node npm the_silver_searcher \
@@ -16,4 +18,7 @@ sudo usermod -aG docker user
 sudo pip install docker-compose --upgrade --user
 sudo pip install awscli --upgrade --user
 curl -L http://get.oh-my.fish > install; fish install --noninteractive; rm -f install
+sudo mkdir -p /usr/share/fonts/inconsolata && cd /usr/share/fonts/inconsolata && \
+  sudo wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Inconsolata/complete/Inconsolata%20Nerd%20Font%20Complete.otf
+sudo fc-cache -s
 
